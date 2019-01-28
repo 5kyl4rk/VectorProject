51524dbf083f72b391f54b2f0e0b5f78d3613a1d
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2019-01-28 13:23:46 -0700

    started assignment

---
+++ VectorProject/Controller/Controller.cpp
@@ -0,0 +1,19 @@
+//
+//  Controller.cpp
+//  VectorProject
+//
+//  Created by Fitzgerald, Skyler on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+Controller :: Controller()
+{
+    
+}
+
+ void Controller :: start()
+{
+    
+}
